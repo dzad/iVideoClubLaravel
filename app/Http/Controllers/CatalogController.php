d4d6351f331d0e07a0fc026f25beda9523ff16cb
MeZayd <zm10@alu.ua.es>
2020-01-21 17:27:02 +0100

    2.1 - Controladores

---
+++ app/Http/Controllers/CatalogController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+{
+  function getHome(){
+    return view("home");
+  }
+
+  function getIndex(){
+    return view("catalog.index");
+  }
+
+  function getShow($id){
+    return view('catalog.show', array("id"=>$id));
+  }
+
+  function getCreate(){
+    return view('catalog.create');
+  }
+
+  function getEdit($id){
+    return view('catalog.edit', array('id' => $id));
+  }
+}
